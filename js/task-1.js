79f576c6d38a2717c5638b67f546665f4bd2412d
Valeria_Karanduyk <lera-ka0195@ukr.net>
2024-08-01 16:08:24 +0300

    commit message

---
+++ js/task-1.js
@@ -0,0 +1,21 @@
+'use strict';
+function isEnoughCapacity(products, containerSize) {
+  let totalProduct = 0;
+  const valueProduct = Object.values(products);
+  for (const addProduct of valueProduct) {
+    totalProduct += addProduct;
+  }
+  if (totalProduct <= containerSize) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
